654707b13aab8fb04bcdd66bba7a5e2181b23116
Noelia Lopez <noelia.l@toptal.com>
2023-04-20 23:03:08 +0200

    Get ContractOptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from "react";
 import VotingContract from "./contracts/Voting.json";
+import { BigNumber, ethers } from "ethers";
 
 declare let window: any;
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "0x581C182EEB327F6FaF4D4a9a982C779029d3129c";
 const contractAbi = VotingContract.abi;
 
 interface Option {
   name: string;
-  url: string;
-  count: number;
+  count: BigNumber;
 }
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [options, setOptions] = useState<Option[]>([]);
+  const [contract, setContract] = useState<any>();
 
   const getEthereumObject = () => {
     if (
@@ -26,14 +27,28 @@ const App = () => {
     return undefined;
   };
 
-  useEffect(() => {
+  const getContract = async () => {
     const ethereum = getEthereumObject();
     if (!ethereum) {
-      console.log("Make sure you have metamask!");
-    } else {
-      console.log("We have the ethereum object", ethereum);
+      console.log("Ethereum object not found");
+      return;
     }
-  }, []);
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+      return contract;
+    } catch (error) {
+      console.log("Contract not deployed to the current network");
+      return undefined;
+    }
+  };
+
+  const getOptions = async (contract: any) => {
+    const options = await contract.getTotalVotesByOption();
+    console.log(options);
+    setOptions(options);
+  };
 
   const connectWallet = async () => {
     try {
@@ -52,17 +67,41 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    const init = async () => {
+      const contract = await getContract();
+      if (!contract) {
+        return;
+      }
+      setContract(contract);
+      getOptions(contract);
+    };
+    init();
+  }, []);
+
+  async function vote(optionIndex: number) {
+    try {
+      const tx = await contract.vote(optionIndex);
+      await tx.wait();
+      console.log('Vote submitted!');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div>
-      {options.map((option) => (
-        <div key={option.name}>
-          <img src={option.url} alt={option.name} />
-          <p>{option.name}</p>
-        </div>
+      {options.map((option, index) => (
+        <li key={index}>
+          {option.name} - {option.count.toString()}
+        </li>
       ))}
       {!currentAccount && (
         <button onClick={connectWallet}>Connect Wallet</button>
       )}
+      {currentAccount && 
+        <button onClick={() => vote(0)}>Vote for Option 0</button>
+      }
     </div>
   );
 };
